8a12b5572f9a07f4eeeae80e021ae1b718937d46
ajmalg08 <ajmalg08@outlook.com>
2019-03-17 05:04:56 -0700

    Sign up with gmail done

--- FinalYearProject/Controllers/FirstController.cs
+++ FinalYearProject/Controllers/FirstController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using FinalYearProject.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +13,12 @@ namespace FinalYearProject.Controllers
     public class FirstController : Controller
     {
         private OPPCContext ORM = null;
+        private IHostingEnvironment ENV = null;
 
-        public FirstController(OPPCContext ORM)
+        public FirstController(OPPCContext ORM, IHostingEnvironment ENV)
         {
             this.ORM = ORM;
+            this.ENV = ENV;
       
 
         }
@@ -24,6 +28,14 @@ namespace FinalYearProject.Controllers
             return View();
         }
 
+
+        public IActionResult ListPage(int Id)
+        {
+            Product S = ORM.Product.Where(m => m.ProductId == Id).FirstOrDefault<Product>();
+
+            return View(S);
+
+        }
 
         public IActionResult ProductDetail(int id)
         {
@@ -61,38 +73,60 @@ namespace FinalYearProject.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult SignUp(UserSystem U)
         {
-            //var userWithSameEmail = ORM.UserSystem.Where(m => m.UserEmail == U.UserEmail).FirstOrDefault(); //checking if the emailid already exits for any user
+            var userWithSameEmail = ORM.UserSystem.Where(m => m.UserEmail == U.UserEmail).FirstOrDefault(); //checking if the emailid already exits for any user
             if (ModelState.IsValid)
             {
-                //if (userWithSameEmail == null)
-                //{
-                try
+                if (userWithSameEmail == null)
                 {
+               
                     ORM.UserSystem.Add(U);
                     ORM.SaveChanges();
-                    ViewBag.Message = "Registration Done";
-                    return RedirectToAction("ProductDetail");
-                }
-                catch (Exception ex)
-                {
 
+                    MailMessage Obj = new MailMessage();
+                    Obj.From = new MailAddress("meharsalman073@gmail.com");
+                    Obj.To.Add(new MailAddress(U.UserEmail));
+                    Obj.Subject = "Welcome to theta Solution:";
+                    Obj.Body = "Dear" + "" + "Mr " + " " + U.UserFname + "<br ><br >" +
+                    "Well Come to Online Product Price Comparison " + "<br><br>" +
+                    "Reguards OPPC Team...";
+                    Obj.IsBodyHtml = true;
+                   
+
+                    //
+
+
+                    //
+
+                    SmtpClient SMTP = new SmtpClient();
+                    SMTP.Host = "smtp.gmail.com";
+                    SMTP.Port = 587;
+                    SMTP.EnableSsl = true;
+                    SMTP.Credentials = new System.Net.NetworkCredential("meharsalman073@gmail.com", "salman123");
+
+                    try
+                    {
+                        SMTP.Send(Obj);
+                    }
+                    catch (Exception)
+                    {
+                        ViewBag.Message = "Mail has sent successfully";
+                    }
+
+
+                    ViewBag.Message = "You are signed in now";
+                    return View();
+                }
+                ViewBag.Message = "Registration Done";
+                    return RedirectToAction("HomePage");
+                
                     
+
                 }
-
-                return View();
-
-                //}
-                //else
-                //{
-                //    ViewBag.Message = "User with this Email Already Exist";
-                //    return View("SignUp");
-                //}
-            }
-
-            else
-            {
-                return View("ProductDetail");
-            }
+                else
+                {
+                    ViewBag.Message = "User with this Email Already Exist";
+                    return View("SignUp");
+                }
         }
 
 
